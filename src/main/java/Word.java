69386ce3c31dba4a8a9c0475790d23ed7cc93899
YHop <ypeng.tw@gmail.com>
2015-08-21 09:05:25 -0700

    Add Word & Definition files. It will be the main java class for this project.

--- src/main/java/Word.java
+++ src/main/java/Word.java
@@ -3,7 +3,46 @@ import java.util.ArrayList;
 public class Word {
   private static ArrayList<Word> instances = new ArrayList<Word>();
 
-  public Category(String name) {
+  private String mWord;
+  private int mId;
+  private ArrayList<Definition> mDefinitions;
 
+  public Word(String word) {
+    mWord = word;
+    instances.add(this);
+    mId = instances.size();
+    mDefinitions = new ArrayList<Definition>();
+  }
+
+  public String getWord() {
+    return mWord;
+  }
+
+  public int getId() {
+    return mId;
+  }
+
+  public ArrayList<Definition> getDefinitions() {
+    return mDefinitions;
+  }
+
+  public void addDefinition(Definition description) {
+    mDefinitions.add(description);
+  }
+
+  public static ArrayList<Word> all() {
+    return instances;
+  }
+
+  public static Word find(int id) {
+    try {
+      return instances.get(id - 1);
+    } catch (IndexOutOfBoundsException exception) {
+      return null;
+    }
+  }
+
+  public static void clear() {
+    instances.clear();
   }
 }
